850b17cb3bdfe5dd62003a33fe0bf2e70acd0a8b
wj0357 <100428719+wj0357@users.noreply.github.com>
2022-02-26 15:45:02 +0800

    Create main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm  the second had change')
+    print_hi('PyCharm  the second hge')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
